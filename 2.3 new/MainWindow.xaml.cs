82319a01b59fae8ac5e4790c948ddf7872d0abe3
Юлия Шепелева <9128051206@mail.ru>
2022-04-02 23:24:23 +0500

    Добавьте файлы проекта.

---
+++ 2.3 new/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2._3_new
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textbox != null)
+            {
+                textbox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            if (textbox != null)
+            {
+                double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+                textbox.FontSize=fontSize;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textbox.FontWeight == FontWeights.Normal)
+            {
+                textbox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                textbox.FontWeight = FontWeights.Normal;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textbox.FontStyle == FontStyles.Normal)
+            {
+                textbox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textbox.FontStyle = FontStyles.Normal;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textbox.TextDecorations.Count == 0)
+            {
+                textbox.TextDecorations.Add(TextDecorations.Underline);
+            }
+            else
+            {
+                textbox.TextDecorations.RemoveAt(0);
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textbox != null)
+            {
+                textbox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textbox != null)
+            {
+                textbox.Foreground = Brushes.Red;
+            }
+        }
+    }
+}
